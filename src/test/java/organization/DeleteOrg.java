23132e51a6c3bcc8c03a8b104b398b7735077100
Bhaskar <Bhaskar@LAPTOP-0USLP61E>
2025-03-08 15:04:25 +0530

    updated delete

--- src/test/java/organization/DeleteOrg.java
+++ src/test/java/organization/DeleteOrg.java
@@ -5,7 +5,7 @@ public class DeleteOrg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Deleted Organization");
-		System.out.println("Validate");
+		System.out.println("Validate org");
 	}
 
 }
