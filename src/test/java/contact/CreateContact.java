9d7b6d5a70fd0cd27c8c2685365bbe51aa364067
keerthana <keerthi0815@gmail.com>
2025-03-08 13:11:21 +0530

    Updated CreateContact class Added Delete Contact class

--- src/test/java/contact/CreateContact.java
+++ src/test/java/contact/CreateContact.java
@@ -5,6 +5,7 @@ public class CreateContact {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Contact Created");
+		System.out.println("Contact updated");
 
 	}
 
---
+++ src/test/java/contact/DeleteContact.java
@@ -0,0 +1,10 @@
+package contact;
+
+public class DeleteContact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Contact Delete");
+	}
+
+}
