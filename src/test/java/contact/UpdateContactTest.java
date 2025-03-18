eb61cbd73d8995ad82ded2be2d533e40b7185e89
keerthanabhaskar11 <keerthi0815@gmail.com>
2025-03-18 15:10:25 +0530

    up

--- src/test/java/contact/UpdateContactTest.java
+++ src/test/java/contact/UpdateContactTest.java
@@ -6,7 +6,8 @@ public class UpdateContactTest
 {
 	@Test
 	public void updateContactTest() {
-		System.out.println("Updated Contact");
+		
+		System.out.println("Updated Contact1");
 		String BROWSER = System.getProperty("browser");
 		String URL = System.getProperty("url");
 		System.out.println(BROWSER);
