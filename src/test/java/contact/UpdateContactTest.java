e7f407ab1e0dc700d7c8cbeb9aa88cfa2258436e
keerthanabhaskar11 <keerthi0815@gmail.com>
2025-03-18 15:04:21 +0530

    update

--- src/test/java/contact/UpdateContactTest.java
+++ src/test/java/contact/UpdateContactTest.java
@@ -7,6 +7,10 @@ public class UpdateContactTest
 	@Test
 	public void updateContactTest() {
 		System.out.println("Updated Contact");
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println(BROWSER);
+		System.out.println(URL);
 	}
 
 }
