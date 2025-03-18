ce63a662d2cfb1f936ce9940883dfb5a9f4cc731
keerthanabhaskar11 <keerthi0815@gmail.com>
2025-03-18 14:57:30 +0530

    UPDATED CREATE contact

--- src/test/java/contact/CreateContactTest.java
+++ src/test/java/contact/CreateContactTest.java
@@ -6,6 +6,10 @@ public class CreateContactTest {
 
 	@Test
 	public void createContactTest() {
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println(BROWSER);
+		System.out.println(URL);
 		// TODO Auto-generated method stub
 		System.out.println("Contact Created");
 		System.out.println("Contact updated");
